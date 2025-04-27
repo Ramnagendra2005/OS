5a1bc01fe841eb7a0384b1eb725159d3455401d2
ramnagendra sripada <168436878+Ramnagendra2005@users.noreply.github.com>
2025-04-28 02:11:10 +0530

    Update sjfnp.c

--- Scheduling Techniques/sjfnp.c
+++ Scheduling Techniques/sjfnp.c
@@ -1,48 +1,59 @@
 #include<stdio.h>
 
 int main() {
-    int n, bt[20], wt[20], tat[20], i, j;
+    int n, bt[20], wt[20], tat[20], at[20], i, j;
     float avgwt = 0, avgtat = 0;
 
     // Input number of processes
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    // Input burst times for each process
-    printf("Enter the burst time of each process:\n");
+    // Input burst times and arrival times for each process
+    printf("Enter the burst time and arrival time of each process:\n");
     for (i = 0; i < n; i++) {
-        printf("Process %d: ", i + 1);
+        printf("Process %d\n", i + 1);
+        printf("Burst time: ");
         scanf("%d", &bt[i]);
+        printf("Arrival time: ");
+        scanf("%d", &at[i]);
     }
 
-    // Sort the burst times in ascending order for SJF
+    // Sort the processes based on arrival time and then by burst time (SJF)
     for (i = 0; i < n - 1; i++) {
         for (j = i + 1; j < n; j++) {
-            if (bt[i] > bt[j]) {
+            if (at[i] > at[j] || (at[i] == at[j] && bt[i] > bt[j])) {
+                // Swap burst times
                 int temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
+                
+                // Swap arrival times
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
             }
         }
     }
 
     // Initialize waiting time for the first process
     wt[0] = 0;
+    
     // Calculate waiting time for each process
     for (i = 1; i < n; i++) {
         wt[i] = 0;
         for (j = 0; j < i; j++) {
             wt[i] += bt[j]; // Waiting time is sum of burst times of all previous processes
         }
+        wt[i] -= at[i]; // Subtract arrival time from total waiting time
     }
 
     // Display process table: Burst Time, Waiting Time, Turnaround Time
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    printf("\nProcess\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
         tat[i] = bt[i] + wt[i]; // Turnaround time = Burst Time + Waiting Time
         avgwt += wt[i];
         avgtat += tat[i];
-        printf("%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], wt[i], tat[i]);
     }
 
     // Calculate and display average waiting time and turnaround time
