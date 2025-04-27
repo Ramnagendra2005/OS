d0a63769b3246dfdd3b8e2446ec059dc4ec00131
ramnagendra sripada <168436878+Ramnagendra2005@users.noreply.github.com>
2025-04-28 02:06:10 +0530

    Update prioritynp.c

--- Scheduling Techniques/prioritynp.c
+++ Scheduling Techniques/prioritynp.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
 
 int main() {
-    int n, bt[20], wt[20], tat[20], pr[20], i, j;
+    int n, bt[20], wt[20], tat[20], pr[20], at[20], i, j;
     float avgwt = 0, avgtat = 0;
 
     // Input number of processes
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    // Input burst times and priorities for each process
-    printf("Enter the burst time and priority of each process:\n");
+    // Input burst times, priorities, and arrival times for each process
+    printf("Enter the burst time, priority, and arrival time of each process:\n");
     for (i = 0; i < n; i++) {
         printf("Process %d\n", i + 1);
         printf("Burst time: ");
         scanf("%d", &bt[i]);
         printf("Priority: ");
         scanf("%d", &pr[i]);
+        printf("Arrival time: ");
+        scanf("%d", &at[i]);
     }
 
     // Sort the processes based on priority (ascending order of priority)
@@ -28,27 +30,35 @@ int main() {
                 temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
             }
         }
     }
 
     // Waiting time for the first process is 0
     wt[0] = 0;
+
     // Calculate waiting time for each process
     for (i = 1; i < n; i++) {
         wt[i] = 0;
         for (j = 0; j < i; j++) {
+            // Ensure waiting time is calculated based on arrival time
+            if (at[i] > wt[j]) {
+                wt[i] = wt[j]; // If the process arrives later, start from its arrival time
+            }
             wt[i] += bt[j]; // Waiting time is sum of burst times of all previous processes
         }
     }
 
     // Display process table: Burst Time, Waiting Time, Turnaround Time
-    printf("\nProcess\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
+    printf("\nProcess\tBurst Time\tPriority\tArrival Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
         tat[i] = bt[i] + wt[i]; // Turnaround time = Burst Time + Waiting Time
         avgwt += wt[i];
         avgtat += tat[i];
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], pr[i], wt[i], tat[i]);
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], pr[i], at[i], wt[i], tat[i]);
     }
 
     // Calculate and display average waiting time and turnaround time
