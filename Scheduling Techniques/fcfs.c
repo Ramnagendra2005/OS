d08dc16434655fefa905eb230b3cf6af97dd82bb
ramnagendra sripada <168436878+Ramnagendra2005@users.noreply.github.com>
2025-04-28 07:17:44 +0530

    Update fcfs.c

--- Scheduling Techniques/fcfs.c
+++ Scheduling Techniques/fcfs.c
@@ -1,94 +1,109 @@
 #include <stdio.h>
 
 int main() {
-    int n, bt[20], wt[20], tat[20], at[20], i, j;
-    float avgwt = 0, avgtat = 0;
-
+    int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    // Input Burst Time
-    printf("Enter the burst time of each process:\n");
-    for (i = 0; i < n; i++) {
-        printf("Process %d: ", i + 1);
-        scanf("%d", &bt[i]);
+    int at[n], bt[n], ct[n], tat[n], wt[n];
+    int p[n]; // process id array
+
+    printf("Enter Arrival Time and Burst Time for each process:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &at[i], &bt[i]);
+        p[i] = i; // initial process id
     }
 
-    // Input Arrival Time
-    printf("Enter the arrival time of each process:\n");
-    for (i = 0; i < n; i++) {
-        printf("Process %d: ", i + 1);
-        scanf("%d", &at[i]);
-    }
-
-    wt[0] = 0; // Waiting time for first process is 0
-    for (i = 1; i < n; i++) {
-        wt[i] = 0;
-        for (j = 0; j < i; j++) {
-            wt[i] += bt[j]; // waiting time is sum of burst times of previous processes
+    // Sort by arrival time
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (at[i] > at[j]) {
+                // Swap arrival time
+                int temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
+                // Swap burst time
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+                // Swap process id
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
         }
-        wt[i] -= at[i]; // Adjust for arrival time
-        if (wt[i] < 0) {
-            wt[i] = 0; // If waiting time is negative, set it to 0
-        }
     }
 
-    // Calculate Turnaround Time
-    printf("\nProcess\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
-    for (i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i]; // Turnaround time = Burst time + Waiting time
-        avgwt += wt[i];
-        avgtat += tat[i];
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], wt[i], tat[i]);
+    // Calculate Completion Time (ct)
+    int crnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (crnt < at[i]) {
+            crnt = at[i]; // CPU idle
+        }
+        crnt += bt[i];
+        ct[i] = crnt;
     }
 
-    avgwt /= n;   // Average Waiting Time
-    avgtat /= n;  // Average Turnaround Time
-
-    // Print average waiting time and turnaround time
-    printf("\nAverage Waiting Time: %.2f", avgwt);
-    printf("\nAverage Turnaround Time: %.2f\n", avgtat);
-
-    // Gantt Chart
-    printf("\nGantt Chart:\n ");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i]; j++) {
-            printf("--");
-        }
-        printf(" ");
+    // Calculate TAT and WT
+    double avg_wt = 0;
+    double avg_tat = 0;
+    for (int i = 0; i < n; i++) {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        avg_wt += wt[i];
+        avg_tat += tat[i];
     }
-    printf("\n|");
 
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i] - 1; j++) {
-            printf(" ");
-        }
-        printf("P%d", i + 1);
-        for (j = 0; j < bt[i] - 1; j++) {
-            printf(" ");
+    avg_wt /= n;
+    avg_tat /= n;
+
+    // Display Table
+    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", p[i]+1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+
+    // Display Gantt Chart
+    printf("\nGantt Chart:\n");
+
+    printf(" |");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < bt[i]; j++) {
+            printf("-");
         }
         printf("|");
     }
-    printf("\n ");
+    printf("\n");
 
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i]; j++) {
-            printf("--");
+    printf(" ");
+    for (int i = 0; i < n; i++) {
+        printf("P%d", p[i]+1);
+        for (int j = 0; j < bt[i]-1; j++) {
+            printf(" ");
         }
         printf(" ");
     }
     printf("\n");
 
-    printf("0");
-    int time = 0;
-    for (i = 0; i < n; i++) {
-        time += bt[i];
-        if (time < 10)
-            printf("  %d", time);  // for aligning single-digit times
-        else
-            printf(" %d", time);
+    printf(" |");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < bt[i]; j++) {
+            printf("-");
+        }
+        printf("|");
     }
     printf("\n");
+
+    printf("%d", (at[0] > 0) ? at[0] : 0);
+    int temp = (at[0] > 0) ? at[0] : 0;
+    for (int i = 0; i < n; i++) {
+        temp = (temp > at[i]) ? temp : at[i];
+        temp += bt[i];
+        printf("\t%d", temp);
+    }
+
+    printf("\n\nAverage Waiting Time = %.2lf", avg_wt);
+    printf("\nAverage Turnaround Time = %.2lf\n", avg_tat);
 
     return 0;
 }
