236137b00bef68de54d3b9cbb355a84894971ae9
ramnagendra sripada <168436878+Ramnagendra2005@users.noreply.github.com>
2025-04-28 09:55:46 +0530

    Update sjfp.c

--- Scheduling Techniques/sjfp.c
+++ Scheduling Techniques/sjfp.c
@@ -3,62 +3,62 @@
 int main() {
     int n, bt[20], rt[20], at[20];
     int wt[20], tat[20], ct[20];
-    int i, sm, t = 0, end;
-    float avgwt = 0, avgtat = 0;
-    int comp = 0;
+    int i, currentTime = 0, completed = 0, minIndex;
+    float totalWT = 0, totalTAT = 0;
 
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    printf("Enter the burst time and arrival time of each process:\n");
+    printf("Enter the burst time and arrival time for each process:\n");
     for (i = 0; i < n; i++) {
         printf("Process %d\n", i + 1);
         printf("Burst time: ");
         scanf("%d", &bt[i]);
         printf("Arrival time: ");
         scanf("%d", &at[i]);
-        rt[i] = bt[i];
+        rt[i] = bt[i]; // Initialize remaining time
     }
 
     printf("\nGantt Chart:\n|");
 
-    while (comp != n) {
-        sm = -1;
+    while (completed != n) {
+        minIndex = -1;
+
         for (i = 0; i < n; i++) {
-            if (rt[i] > 0 && at[i] <= t) {
-                if (sm == -1 || rt[i] < rt[sm])
-                    sm = i;
+            if (rt[i] > 0 && at[i] <= currentTime) {
+                if (minIndex == -1 || rt[i] < rt[minIndex]) {
+                    minIndex = i;
+                }
             }
         }
 
-        if (sm == -1) {
-            t++;
+        if (minIndex == -1) {
+            currentTime++;
             continue;
         }
 
-        printf(" P%d |", sm + 1);
+        printf(" P%d |", minIndex + 1);
 
-        rt[sm]--;
-        t++;
+        rt[minIndex]--;
+        currentTime++;
 
-        if (rt[sm] == 0) {
-            comp++;
-            end = t;
-            ct[sm] = end;
-            tat[sm] = ct[sm] - at[sm];
-            wt[sm] = tat[sm] - bt[sm];
+        if (rt[minIndex] == 0) {
+            completed++;
+            ct[minIndex] = currentTime;
+            tat[minIndex] = ct[minIndex] - at[minIndex];
+            wt[minIndex] = tat[minIndex] - bt[minIndex];
+            totalWT += wt[minIndex];
+            totalTAT += tat[minIndex];
         }
     }
 
-    printf("\n\nProcess\tBT\tAT\tCT\tWT\tTAT\n");
+    printf("\n\nProcess\tBurst Time\tArrival Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
-        avgwt += wt[i];
-        avgtat += tat[i];
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, bt[i], at[i], ct[i], wt[i], tat[i]);
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], ct[i], wt[i], tat[i]);
     }
 
-    printf("\nAverage Waiting Time: %.2f\n", avgwt / n);
-    printf("Average Turnaround Time: %.2f\n", avgtat / n);
+    printf("\nAverage Waiting Time: %.2f\n", totalWT / n);
+    printf("Average Turnaround Time: %.2f\n", totalTAT / n);
 
     return 0;
 }
