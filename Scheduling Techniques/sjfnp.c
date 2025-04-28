d79808bfdba94b77f8a8d827c5f42dc9f5e56945
ramnagendra sripada <168436878+Ramnagendra2005@users.noreply.github.com>
2025-04-28 07:45:26 +0530

    Update sjfnp.c

--- Scheduling Techniques/sjfnp.c
+++ Scheduling Techniques/sjfnp.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
 int main() {
-    int n, bt[20], wt[20], tat[20], at[20], i, j;
+    int n, bt[20], wt[20], tat[20], at[20], ct[20], i, j;
     float avgwt = 0, avgtat = 0;
 
-    // Input number of processes
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    // Input burst times and arrival times for each process
     printf("Enter the burst time and arrival time of each process:\n");
     for (i = 0; i < n; i++) {
         printf("Process %d\n", i + 1);
@@ -18,16 +16,14 @@ int main() {
         scanf("%d", &at[i]);
     }
 
-    // Sort the processes based on arrival time and then by burst time (SJF)
+    // Sort based on arrival time and burst time
     for (i = 0; i < n - 1; i++) {
         for (j = i + 1; j < n; j++) {
             if (at[i] > at[j] || (at[i] == at[j] && bt[i] > bt[j])) {
-                // Swap burst times
                 int temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
-                
-                // Swap arrival times
+
                 temp = at[i];
                 at[i] = at[j];
                 at[j] = temp;
@@ -35,71 +31,54 @@ int main() {
         }
     }
 
-    // Initialize waiting time for the first process
-    wt[0] = 0;
-    
-    // Calculate waiting time for each process
-    for (i = 1; i < n; i++) {
-        wt[i] = 0;
-        for (j = 0; j < i; j++) {
-            wt[i] += bt[j]; // Waiting time is sum of burst times of all previous processes
-        }
-        wt[i] -= at[i]; // Subtract arrival time from total waiting time
+    int time = 0;
+    for (i = 0; i < n; i++) {
+        if (time < at[i])
+            time = at[i]; // CPU idle till process arrives
+
+        time += bt[i];
+        ct[i] = time;
     }
 
-    // Display process table: Burst Time, Waiting Time, Turnaround Time
-    printf("\nProcess\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
     for (i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i]; // Turnaround time = Burst Time + Waiting Time
+        tat[i] = ct[i] - at[i]; // Turnaround time = Completion Time - Arrival Time
+        wt[i] = tat[i] - bt[i]; // Waiting time = Turnaround Time - Burst Time
+
         avgwt += wt[i];
         avgtat += tat[i];
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], wt[i], tat[i]);
     }
 
-    // Calculate and display average waiting time and turnaround time
+    printf("\nProcess\tBurst Time\tArrival Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], at[i], ct[i], wt[i], tat[i]);
+    }
+
     avgwt /= n;
     avgtat /= n;
     printf("\nAverage Waiting Time: %.2f", avgwt);
     printf("\nAverage Turnaround Time: %.2f\n", avgtat);
 
     // Gantt Chart
-    printf("\nGantt Chart:\n");
-    printf(" ");
+    printf("\nGantt Chart:\n ");
     for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i]; j++) {
-            printf("--");
-        }
-        printf(" ");
+        printf("----");
     }
-    printf("\n|");
+    printf("-\n|");
     for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i] - 1; j++) {
-            printf(" ");
-        }
-        printf("P%d", i + 1); // Process display
-        for (j = 0; j < bt[i] - 1; j++) {
-            printf(" ");
-        }
-        printf("|");
+        printf(" P%d |", i + 1);
     }
     printf("\n ");
     for (i = 0; i < n; i++) {
-        for (j = 0; j < bt[i]; j++) {
-            printf("--");
-        }
-        printf(" ");
+        printf("----");
     }
-    printf("\n");
+    printf("-\n");
 
-    // Display completion times in the Gantt chart
     printf("0");
-    int time = 0;
     for (i = 0; i < n; i++) {
-        time += bt[i];
-        if (time < 10)
-            printf("  %d", time);  // Aligning single-digit times
+        if (ct[i] < 10)
+            printf("   %d", ct[i]);
         else
-            printf(" %d", time);
+            printf("  %d", ct[i]);
     }
     printf("\n");
 
